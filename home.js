d0c1f976bc2dd6eb3b0c0c2b8b77a6d9f16c2139
school-s-debug <Dan570140@gmail.com>
2026-02-01 14:24:06 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+let slides = document.querySelectorAll('.slides');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 4000);
